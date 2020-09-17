9e3aec024a49383f8d75a73278672374f16685b4
NessMC <contact@nessmc.fr>
2020-09-17 19:53:45 +0100

    ✨ Basic tokens added.

--- src/core/tokens/tokens.ts
+++ src/core/tokens/tokens.ts
@@ -2,3 +2,9 @@
                Javaskript
                  Tokens
 //////////////////////////////////////*/
+
+export default {
+  TABS: /\s+?/g, // Match tabs in code
+  STRING: /".*?"/g, // Match string and its content.
+  PRINT: /print/, // Match print keyword.
+};
