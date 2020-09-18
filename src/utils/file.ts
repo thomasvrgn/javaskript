ff5aec49b53e89d24d96bc881ee93e1bd6671a6f
NessMC <contact@nessmc.fr>
2020-09-18 19:43:38 +0200

    ✨ File utils class added.

--- src/utils/file.ts
+++ src/utils/file.ts
@@ -2,3 +2,22 @@
                Javaskript
                   File
 //////////////////////////////////////*/
+
+import { readFile } from 'fs';
+
+export default class File {
+  private fileName: string;
+
+  constructor(fileName: string) {
+    this.fileName = fileName;
+  }
+
+  public read(): Promise<string> {
+    return new Promise((resolve: Function, reject: Function) => {
+      readFile(this.fileName, 'utf-8', (error: NodeJS.ErrnoException, data: string) => {
+        if (error) reject(error);
+        resolve(data);
+      });
+    });
+  }
+}
