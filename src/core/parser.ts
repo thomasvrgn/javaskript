6bed108e6c446f213d496608567ba094b0e3bc42
NessMC <contact@nessmc.fr>
2020-11-15 12:26:32 +0100

    ✨ Print parsing added.

--- src/core/parser.ts
+++ src/core/parser.ts
@@ -23,16 +23,27 @@ export default class Parser {
     this.tokens = Lexer.tokenize(this.content);
   }
 
+  protected print(tokens: Token[], index: number, ast: Node, token: Token) {
+    ast.children.push({
+      type: Types.FunctionCall,
+      raw: token.value,
+      children: [],
+      parent: ast,
+    });
+    this.Any(this.tokens, index + 1, this.ast, this.tokens[index + 1]);
+  }
+
   // eslint-disable-next-line class-methods-use-this
   private Any(tokens: Token[], index: number, ast: Node, token: Token): null {
     const fn: Function | undefined = this[token.value.toLocaleLowerCase()];
     if (!fn) return null;
-    this[token.value.toLocaleLowerCase()](this.tokens, index + 1, ast, this.tokens[index + 1]);
+    this[token.value.toLocaleLowerCase()](this.tokens, index, ast, this.tokens[index]);
     return null;
   }
 
   public parse(): string {
     this.Any(this.tokens, 0, this.ast, this.tokens[0]);
+    console.log(this.ast);
     return this.content;
   }
 }
